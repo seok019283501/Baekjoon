e7172de1bc6513f3c0c4b082ee35a1d63891e49f
민석 <47300870+seok019283501@users.noreply.github.com>
2024-05-26 17:27:11 +0900

    [level 2] Title: 전화번호 목록, Time: 251.24 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        int size = phone_book.length;
+        for(int i = 0; i < size-1; i++){
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
+        }
+        return answer;
+    }
+}
